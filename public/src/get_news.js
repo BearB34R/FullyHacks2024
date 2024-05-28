5301f81acc39079fbb5444f897abe343d2dedafd
BearB34R <andynguyendo@csu.fullerton.edu>
2024-05-27 17:43:45 -0700

    testing

--- public/src/get_news.js
+++ public/src/get_news.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+
 const urlParams = new URLSearchParams(window.location.search);
 window.myParam = urlParams.get("q"); // set it as a global variable
 
